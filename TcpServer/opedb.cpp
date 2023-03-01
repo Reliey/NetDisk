11c6dfd4c07b3ffbc2b39907d4d93bfbbb3e2b0c
Reliey <616318745@qq.com>
2023-03-01 15:34:33 +0800

    test

--- TcpServer/opedb.cpp
+++ TcpServer/opedb.cpp
@@ -14,7 +14,7 @@ OpeDB &OpeDB::getInstance()
 void OpeDB::init()
 {
     m_db.setHostName("localhost");
-    m_db.setDatabaseName("E:\\QT_project\\TcpServer\\cloud.db");
+    m_db.setDatabaseName("C:\\Users\\lenovo\\Desktop\\NetDisk\\TcpServercloud.db");
     if(m_db.open())
     {
         QSqlQuery query;
